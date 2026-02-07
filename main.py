6c78ea706bc7442c25561c96c0a4a22280ea7537
glacier54 <screitsma@proton.me>
2026-02-07 14:17:36 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+from to_ascii import main as to_ascii
+import requests
+import random
+from bs4 import BeautifulSoup
+import shutil
+import os, tomllib
+from pathlib import Path
+from platformdirs import user_config_dir
+import argparse
+
+
+
+# https://github.com/ClaustAI/r34-api/blob/main/app.py
+def ellips(s, mx):
+    if len(s) > mx:
+        return s[:mx-3]+'...'
+    return s
+
+def get_images(auth, tags):
+    url = f'https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&limit=100&pid=1&tags={tags}&json=1&'+auth
+    r = requests.get(url).json()
+    return random.choice(r)
+def main(obj, ma):
+    img_bytes = requests.get(obj['preview_url']).content
+    w, _ = to_ascii(img_bytes, (int(ma[0]), int(ma[1]-4)))
+    print(f"https://rule34.xxx/index.php?page=post&s=view&id={obj['id']}")
+    print(obj['owner'])
+    print(ellips(obj['tags'], w+3))
+    print(f"score: {obj['score']}")
+if __name__ == '__main__':
+    size = shutil.get_terminal_size(fallback=(60, 24))
+    parser = argparse.ArgumentParser(description="Example with optional args")
+    parser.add_argument('--max-columns', '-c', type=int, default=size.columns//2, help='Max character columns. Defaults to 1/2 terminal width.')
+    parser.add_argument('--max-rows', '-r', type=int, default=size.lines-4, help='Max character rows. Defaults to terminal height.')
+    args = parser.parse_args()
+    path = Path(user_config_dir("goonfetch")) / "config.toml"
+    if not path.exists:
+        print("No configuration file detected.")
+        exit
+    cfg = tomllib.loads(path.read_text())
+    if not cfg.get('auth'):
+        print("No auth found. You can create an api-key and find your user id in the rule34.xxx account settings page.")
+    auth = cfg.get('auth')
+    main(get_images(auth, cfg.get('tags')), (args.max_columns, args.max_rows+4))
---
+++ to_ascii.py
@@ -0,0 +1,59 @@
+import cv2
+import numpy as np
+
+def ansi(x, fg, bg=None, isBold=False):
+    parts = []
+
+    # bold on/off (no full reset)
+    parts.append("1" if isBold else "22")
+
+    # foreground RGB
+    fr, fg_, fb = fg
+    parts.append(f"38;2;{fr};{fg_};{fb}")
+
+    # background RGB (optional)
+    if bg is not None:
+        br, bg_, bb = bg
+        parts.append(f"48;2;{br};{bg_};{bb}")
+
+    sgr = ";".join(parts)
+
+    return f"\033[{sgr}m{x}\033[0m"
+
+
+chars = list(reversed("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`\'. "))
+def main(imbytes, rc):
+    maw, mah = rc
+    maw -= 3
+    arr = np.frombuffer(imbytes, dtype=np.uint8)
+    img = cv2.imdecode(arr, cv2.IMREAD_COLOR)
+    h_o, w_o = img.shape[:2]
+    if h_o*0.55/mah > w_o/maw:
+        w, h = int(w_o*mah/h_o/0.55), mah
+    else:
+        w, h = maw, int(h_o*maw/w_o*0.55)
+    img = cv2.resize(img, (w, h), interpolation=cv2.INTER_LINEAR)
+    wts = np.array([77, 150, 29], dtype=np.uint16)  # uint16 to avoid overflow
+    dist = np.sum(img.astype(np.uint16) * wts, axis=2) >> 8  # integer division by 256
+    dist = dist.astype(np.uint8)  # optional: back to uint8
+    mi, ma = np.percentile(dist, 10), np.percentile(dist, 90)
+
+    n = len(chars)*2
+    idx = np.clip(np.uint8((dist-mi)*n/ma), 0, n-1)
+    for i in range(h):
+        s = ''
+        for j in range(w):
+            # bb, bg, br = img[i, j]
+            fb, fg, fr = img[i, j]
+            x_o = idx[i, j]
+            # fr, fg, fb = 255, 255, 255
+            br, bg, bb = 0, 0, 0
+            x = chars[x_o//2]
+            isBold = x_o % 2 == 1
+            s += ansi(x, (fr, fg, fb), isBold=isBold)
+        print(f'{i:02d} '+s)
+    return w, h
+if __name__ == '__main__':
+    with open('image.png', 'rb') as f:
+        by = f.read()
+    main(by, (60, 60*0.55))
