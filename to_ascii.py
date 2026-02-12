609841d76eebb648e0daa65192bb872d1608179f
glacier54 <260065278+glacier54@users.noreply.github.com>
2026-02-12 15:43:53 +0100

    disabled bold and changed back to euclidean distance for better ascii result

--- to_ascii.py
+++ to_ascii.py
@@ -23,7 +23,7 @@ def ansi(x, fg, bg=None, isBold=False):
     return f"\033[{sgr}m{x}\033[0m"
 
 
-chars = list("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`\'. ")
+chars = list(reversed("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`\'. "))
 def main(imbytes, rc):
     maw, mah = rc
     maw -= 3
@@ -34,13 +34,12 @@ def main(imbytes, rc):
     imag = imag.resize((imag.width, int(imag.height * 0.55)), Image.BILINEAR)
     img = np.array(imag.convert('RGB'), dtype='uint8')
     
-    wts = np.array([77, 150, 29], dtype=np.uint16)  # uint16 to avoid overflow
+    wts = np.array([255, 255, 255], dtype=np.uint16)  # uint16 to avoid overflow
     dist = np.sum(img.astype(np.uint16) * wts, axis=2) >> 8  # integer division by 256
     dist = dist.astype(np.uint8)  # optional: back to uint8
-    mi, ma = np.percentile(dist, 10), np.percentile(dist, 90)
 
     n = len(chars)*2
-    idx = np.clip(np.uint8((dist-mi)*n/ma), 0, n-1)
+    idx = np.clip(dist//3, 0, n-1)
     for i in range(img.shape[0]):
         s = ''
         for j in range(img.shape[1]):
@@ -51,7 +50,7 @@ def main(imbytes, rc):
             br, bg, bb = 0, 0, 0
             x = chars[x_o//2]
             isBold = x_o % 2 == 1
-            s += ansi(x, (fr, fg, fb), isBold=isBold)
+            s += ansi(x, (fr, fg, fb), isBold=False)
         print(f'{i:02d} '+s)
     return img.shape[:-1][::-1]
 if __name__ == '__main__':
